c5077813d2bef5fffc470ff44f6fd2568d962583
kamdem-arielle <kamdemarielle.y@gmail.com>
2021-10-07 00:52:05 +0100

    new

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -21,7 +21,7 @@ const Register=({onroutechange})=>{
             </div>
             </fieldset>
             <div className="">
-            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" onClick={()=>{onroutechange()}}/>
+            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" onClick={()=>{onroutechange('home')}}/>
             </div>
         </div>
         </main>
